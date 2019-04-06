46b51fb919a82f3a8746e6d29f6c9fc10832e41f
Max Staff <max.staff@gmx.de>
2019-04-07 00:58:14 +0200

    Vier Gewinnt ist in einem akzeptablen Zustand.

--- vier-gewinnt.cpp
+++ vier-gewinnt.cpp
@@ -5,10 +5,11 @@
 #include <limits>
 #include <sstream>
 #include <tuple>
+#include <unordered_map>
 #include <vector>
 
 #define MAXINT std::numeric_limits<unsigned long int>::max()
-bool playerWins(const std::uint64_t& player);
+bool playerWins(const std::uint64_t& player, const std::uint64_t& lastMove);
 
 struct Field {
 	std::uint64_t player1 = 0;
@@ -21,25 +22,21 @@ struct Field {
 		player2 = tmp;
 	}
 
-	bool play(std::uint8_t move) {
-		assert(__builtin_popcount(move) == 1 && move < (1 << 7));
+	std::uint64_t play(std::uint8_t move) {
+		assert(__builtin_popcount(move) == 1);
+		assert(move < (1 << 7));
 		std::uint64_t offsetMove = move;
 		for (auto i = 0; i < 6 && (offsetMove & player1) == 0 && (offsetMove & player2) == 0; ++i) {
 			offsetMove <<= 7;
 		}
 		offsetMove >>= 7;
-		if (offsetMove == 0) {
-			return false;
-		}
 		player1 |= offsetMove;
 		assert((player1 & player2) == 0);
-		return true;
+		return offsetMove;
 	}
 
 	std::string print() {
 		std::ostringstream tmp;
-		tmp << std::bitset<42>(player1) << "\n";
-		tmp << std::bitset<42>(player2) << "\n";
 		tmp << "║1234567║\n";
 		for (auto i = 0; i < 6; ++i) {
 			tmp << "║";
@@ -62,62 +59,54 @@ struct Field {
 		return tmp.str();
 	}
 
-	template<std::size_t depth, class T>
-	bool getScore(T storage) {
-		if (playerWins(player1)) {
-			return true;
-		}
-		Field tmp{*this};
-		tmp.reverse();
-		for (auto i = 0; i < 7; ++i) {
-			*std::next(storage, i) = tmp;
-			if constexpr (depth > 16) {
-				return false;
-			} else {
-				if (std::next(storage, i)->play(1 << i) && std::next(storage, i)->template getScore<depth + 1>(std::next(storage, 7))) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-
-	void botMove() {
-		/*Field tmp{*this};
-		// Field, move (one-hot), score (moves until won/lost)
-		std::vector<std::tuple<Field, std::uint_fast8_t, std::int_fast8_t>> storage{49ul - __builtin_popcountll(player1) - __builtin_popcountll(player2), {tmp, 0, 0}};
-		auto pos = std::next(storage.begin());
-		while (pos != storage.begin()) {
-			std::get<0>(*pos).play(std::get<1>(*pos));
-			if (playerWins(std::get<0>(*pos).player1)) {
-				std::advance(pos, -1);
-				std::get<2>(*pos) = -1;
-			} else if (playerWins(std::get<0>(*pos).player2)) {
-				std::advance(pos, -1);
-				std::get<2>(*pos) = -1;
-			}
-		}*/
-
-		Field tmp{*this};
-		std::vector<Field> fields{(49ul - __builtin_popcountll(player1) - __builtin_popcountll(player2)) * 7, tmp};
-		for (auto i = 0; i < 7; ++i) {
-			std::cerr << "\r" << (0 + 1.0/7 * i) * 100 << "%  ";
-			if (fields[i].play(1 << i) && fields[i].getScore<1>(std::next(fields.begin(), 7))) {
-				play(1 << i);
-				return;
-			}
-		}
-		for (auto i = 0; i < 7; ++i) {
-			if (fields[i] != tmp) {
-				play(1 << i);
-				return;
-			}
-		}
-	}
+	std::uint64_t botMove();
 
 	friend bool operator==(Field f1, Field f2);
 	friend bool operator!=(Field f1, Field f2);
 };
+
+struct ScoredMove {
+	Field field;
+	std::uint8_t currentMove;
+	std::uint8_t bestMove;
+	std::uint8_t score;
+};
+
+ScoredMove bestMove(Field field, std::uint8_t depth = 0) {
+	if (depth > 10) {
+		return ScoredMove{field, 0, 0, static_cast<std::uint8_t>(-1)};
+	}
+	field.reverse();
+	ScoredMove result;
+	result.field = field;
+	result.score = 0;
+	for (result.currentMove = 1; result.currentMove < (1 << 7); result.currentMove <<= 1) {
+		std::uint64_t currentPos = result.field.play(result.currentMove);
+		if (currentPos) {
+			if (playerWins(result.field.player1, currentPos)) {
+				result.bestMove = result.currentMove;
+				result.score = static_cast<std::uint8_t>(-1);
+				return result;
+			}
+			std::uint8_t currentScore = -bestMove(result.field, depth + 1).score;
+			if (currentScore < 128) {
+				++currentScore;
+			} else {
+				--currentScore;
+			}
+			if (currentScore > result.score) {
+				result.bestMove = result.currentMove;
+				result.score = currentScore;
+			}
+		}
+		result.field = field;
+	}
+	return result;
+}
+
+std::uint64_t Field::botMove() {
+	return play(bestMove(*this).bestMove);
+}
 
 bool operator==(Field f1, Field f2) {
 	return f1.player1 == f2.player1 && f1.player2 == f2.player2;
@@ -127,63 +116,54 @@ bool operator!=(Field f1, Field f2) {
 	return f1.player1 != f2.player1 || f1.player2 != f2.player2;
 }
 
-bool playerWins(const std::uint64_t& player) {
+bool playerWins(const std::uint64_t& player, const std::uint64_t& lastMove) {
+	const auto offset = __builtin_ctzll(lastMove) % 7;
 	// vertical
-	for (std::uint_fast8_t i = 0; i < 7; ++i) {
-		if (player & (0b1ull << (21 + i)) && player & (0b1ull << (14 + i)) && __builtin_popcountll(player & (0b100000010000001000000100000010000001ull << i)) > 3) {
-			if (player & (0b1ull << (7 + i))) {
-				if (player & (0b1ull << i) || player & (0b1ull << (28 + i))) {
-					return true;
-				}
-			} else if (player & (0b1ull << (28 + i)) && player & (0b1ull << (35 + i))) {
+	if (player & (lastMove << 7)) {
+		if (player & (lastMove << 14)) {
+			if (player & (lastMove << 21) || player & (lastMove >> 7)) {
 				return true;
 			}
+		} else if (player & (lastMove >> 7) && player & (lastMove >> 14)) {
+			return true;
 		}
+	} else if (player & (lastMove >> 7) && player & (lastMove >> 14) && player & (lastMove >> 21)) {
+		return true;
 	}
 	// horizontal
-	for (std::uint_fast8_t i = 0; i < 42; i += 7) {
-		if (player & (0b1000ull << i) && __builtin_popcountll(player & (0b1111111ull << i)) > 3) {
-			if (player & (0b100ull << i)) {
-				if (player & (0b10ull << i)) {
-					if (player & (0b1ull << i) || player & (0b10000ull << i)) {
-						return true;
-					}
-				} else if (player & (0b10000ull << i) && player & (0b100000ull << i)) {
-					return true;
-				}
-			} else if (player & (0b10000ull << i) && player & (0b100000ull << i) && player & (0b1000000ull << i)) {
+	if (offset < 7 && player & (lastMove << 1)) {
+		if (offset < 6 && player & (lastMove << 2)) {
+			if ((offset < 5 && player & (lastMove << 3)) || (offset > 0 && player & (lastMove >> 1))) {
 				return true;
 			}
+		} else if (offset > 1 && player & (lastMove >> 1) && player & (lastMove >> 2)) {
+			return true;
 		}
+	} else if (offset > 2 && player & (lastMove >> 1) && player & (lastMove >> 2) && player & (lastMove >> 3)) {
+		return true;
 	}
 	// diagonal
-	for (std::uint_fast8_t i = 0; i < 42; i += 7) {
-		if (player & (0b1000ull << i) && __builtin_popcountll(player & (0b1111111ull << i)) > 3) {
-			if (i < 35 && player & (0b100ull << (i + 7))) {
-				if (i < 28 && player & (0b10ull << (i + 14))) {
-					if ((i < 21 && player & (0b1ull << (i + 21))) || (i > 0 && player & (0b10000ull << (i - 7)))) {
-						return true;
-					}
-				} else if (i > 7 && player & (0b10000ull << (i - 7)) && player & (0b100000ull << (i - 14))) {
-					return true;
-				}
-			} else if (i > 14 && player & (0b10000ull << (i - 7)) && player & (0b100000ull << (i - 14)) && player & (0b1000000ull << (i - 21))) {
+	if (offset < 7 && player & (lastMove << 8)) {
+		if (offset < 6 && player & (lastMove << 16)) {
+			if ((offset < 5 && player & (lastMove << 24)) || (offset > 0 && player & (lastMove >> 8))) {
 				return true;
 			}
+		} else if (offset > 1 && player & (lastMove >> 8) && player & (lastMove >> 16)) {
+			return true;
 		}
-		if (player & (0b1000ull << i) && __builtin_popcountll(player & (0b1111111ull << i)) > 3) {
-			if (i > 0 && player & (0b100ull << (i - 7))) {
-				if (i > 7 && player & (0b10ull << (i - 14))) {
-					if ((i > 14 && player & (0b1ull << (i - 21))) || (i < 35 && player & (0b10000ull << (i + 7)))) {
-						return true;
-					}
-				} else if (i < 28 && player & (0b10000ull << (i + 7)) && player & (0b100000ull << (i + 14))) {
-					return true;
-				}
-			} else if (i < 21 && player & (0b10000ull << (i + 7)) && player & (0b100000ull << (i + 14)) && player & (0b1000000ull << (i + 21))) {
+	} else if (offset > 2 && player & (lastMove >> 8) && player & (lastMove >> 16) && player & (lastMove >> 24)) {
+		return true;
+	}
+	if (offset < 7 && player & (lastMove >> 6)) {
+		if (offset < 6 && player & (lastMove >> 12)) {
+			if ((offset < 5 && player & (lastMove >> 18)) || (offset > 0 && player & (lastMove << 6))) {
 				return true;
 			}
+		} else if (offset > 1 && player & (lastMove << 6) && player & (lastMove << 12)) {
+			return true;
 		}
+	} else if (offset > 2 && player & (lastMove << 6) && player & (lastMove << 12) && player & (lastMove << 18)) {
+		return true;
 	}
 	return false;
 }
@@ -191,21 +171,29 @@ bool playerWins(const std::uint64_t& player) {
 int main() {
 	Field field;
 	int input;
-	while (!playerWins(field.player1) && !playerWins(field.player2)) {
+	std::uint64_t currentPlayerMove;
+	std::uint64_t currentBotMove;
+	do {
 		field.reverse();
 		std::cout << field.print();
 		std::cin >> input;
-		while (!field.play(1 << (input - 1))) {
+		while ((input < 1 || input > 7 || !(currentPlayerMove = field.play(1 << (input - 1)))) && __builtin_popcountll(field.player1 | field.player2) < 49) {
 			std::cin >> input;
 		}
 		field.reverse();
-		field.botMove();
-	}
+		currentBotMove = field.botMove();
+		if (!currentBotMove) {
+			std::cerr << "Internal error.\n";
+			return 0;
+		}
+	} while (!playerWins(field.player1, currentBotMove) && !playerWins(field.player2, currentPlayerMove) && __builtin_popcountll(field.player1 | field.player2) < 49);
 	field.reverse();
-	if (playerWins(field.player1)) {
+	if (playerWins(field.player1, currentPlayerMove)) {
 		std::cout << "Congratulations! You won!\n";
-	} else if (playerWins(field.player2)) {
+	} else if (playerWins(field.player2, currentBotMove)) {
 		std::cout << "Sorry, you lost.\n";
+	} else {
+		std::cout << "It seems to be a draw.\n";
 	}
 	std::cout << field.print();
 	return 0;
